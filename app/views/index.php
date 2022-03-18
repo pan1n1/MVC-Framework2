4d0476c32b8318b2820d86ee57db73732ce3a1cd
Dr1ftr <72500766+Dr1ftr@users.noreply.github.com>
2022-03-18 11:24:52 +0100

    Setup

---
+++ app/views/index.php
@@ -0,0 +1,11 @@
+<?php
+/* This is how you echo out database information on the screen
+foreach ($data['users'] as $user) {
+    echo "Information: " . $user->user_name . $user->user_email;
+    echo "<br>";
+}
+*/
+?>
+HOME PAGE
+<br>
+<a href="http://mvc-toets.org/countries/index">Countries</a>
